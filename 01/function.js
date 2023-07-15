5aaf93dd13992fbe3f19ae1cb4179324259cb0d8
Mehedi Jaman <mail4mjaman@gmail.com>
2023-07-15 12:28:34 +0600

    - Teaching JS 01 updated

---
+++ 01/function.js
@@ -0,0 +1,24 @@
+function sum(n1,n2){    
+    return n1+n2
+}
+
+function sub(n1,n2){
+    return n1 - n2
+}
+
+function mul(n1, n2){
+    return n1 * n2
+}
+
+function div(n1,n2){
+    return n1 / n2
+}
+
+const sum1 = sum(12,13)
+const sum2 = sum(12,15)
+
+console.log(sum1)
+// sum(12,150)
+// sub(12,5)
+// mul(12,10)
+// div(100,5)
---
+++ 01/index.js
@@ -0,0 +1,51 @@
+// let name = "Mehedi Jaman"
+// const pi = 3.1416
+
+// console.log("Hello " + name);
+
+// const a = '15';
+// const b = '13';
+
+// // let sum = parseInt(a) + parseInt(b);
+
+// let sum = a*1 + b*1
+
+// console.log(sum)
+
+// const numberOne = 23;  // Studyly case
+// const numberTwo = 4; // Camel case
+// let result;
+
+// Data types
+// - String
+// - Number
+// - Object 
+// - Boolean
+
+// let status = false;
+// console.log(status)
+
+// Concetanation
+// const firstName = "Mehedi"
+// const lastName = "Jaman"
+
+// using concetanation
+// const fullName = firstName +" "+ lastName
+
+// using template literals
+// const fullName = "My name is " + firstName + " " + lastName
+
+// const fullName = `My name is ${firstName} ${lastName}`
+
+// console.log(fullName)
+
+// const numberOne = 35;
+// const numberTwo = 34;
+
+// const sum = `The summation of ${numberOne} and ${numberTwo} is ${numberOne + numberTwo}`
+
+// console.log(numberOne)
+
+// let number = '76'
+// number = parseFloat(number)
+// console.log(typeof number)
